1bf00ab81c09330e87c3001a99885f3ab18bfc9e
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-20 12:36:52 +0200

    Update app.py

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def index():
     return 'Hello, World!'
 
+@app.route('/<name>')
+def print_name(name):
+    return 'Hi , {}'.format(name)
+
 if __name__ == '__main__':
     app.run()
