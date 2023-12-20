e5f4a17c77e332847bae6214a4bdc3f6d89409c9
Oscar Byiringiro <byiringoroscar@gmail.com>
2023-12-20 14:11:56 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,14 +1,41 @@
-from flask import Flask
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
 
-@app.route('/')
-def index():
-    return 'Hello, World!'
+books_list = [
+    {"id": 1, "author": "Haruki Murakami", "language": "English", "title": "Norwegian Wood"},
+    {"id": 2, "author": "Jane Austen", "language": "English", "title": "Pride and Prejudice"},
+    {"id": 3, "author": "Gabriel García Márquez", "language": "Spanish", "title": "One Hundred Years of Solitude"},
+    {"id": 4, "author": "Fyodor Dostoevsky", "language": "Russian", "title": "Crime and Punishment"},
+    {"id": 5, "author": "J.K. Rowling", "language": "English", "title": "Harry Potter and the Sorcerer's Stone"},
+    {"id": 6, "author": "Kazuo Ishiguro", "language": "English", "title": "Never Let Me Go"},
+    {"id": 7, "author": "George Orwell", "language": "English", "title": "1984"},
+    {"id": 8, "author": "Leo Tolstoy", "language": "Russian", "title": "War and Peace"},
+    {"id": 9, "author": "Ernest Hemingway", "language": "English", "title": "The Old Man and the Sea"},
+    {"id": 10, "author": "Toni Morrison", "language": "English", "title": "Beloved"}
+]
 
-@app.route('/<name>')
-def print_name(name):
-    return 'Welcome , {}'.format(name)
+
+@app.route('/books', methods=['GET', 'POST'])
+def books():
+    if request.method == 'GET':
+        if len(books_list) > 0:
+            return jsonify(books_list)
+        else:
+            'Nothing Found', 404
+    if request.method == 'POST':
+        new_author = request.form['author']
+        new_language = request.form['language']
+        new_title = request.form['title']
+        id = books_list[-1]['id'] + 1
+        new_obj = {
+            'id': id,
+            'author': new_author,
+            'language': new_language,
+            'title': new_title
+        }
+        books_list.append(new_obj)
+        return jsonify(books_list), 201
 
 if __name__ == '__main__':
     app.run(debug=True)
